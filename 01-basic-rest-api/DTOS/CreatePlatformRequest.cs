45988d5b936037b2a5f42b7df73332898b3d7c86
Paul McGinley <paulmcginley16@outlook.com>
2026-08-10 18:29:04 +0100

    feat: dtos updated for resposne, and requests per ACS

---
+++ 01-basic-rest-api/DTOS/CreatePlatformRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicRestApi.Dtos
+{
+    public class CreatePlatformRequest
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Manufacturer { get; set; } = string.Empty;
+
+        [Range(1970, 2100)]
+        public int ReleaseYear { get; set; }
+    }
+}
